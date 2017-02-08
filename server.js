4c53d9ff8ed7436044bc23480289b4bd072f3ece
tmaxwellmt <tmaxwellmt@gmail.com>
2017-02-07 20:22:44 -0700

    set up different API end point

--- server.js
+++ server.js
@@ -1,8 +1,29 @@
-var http = require('http');
+var express = require('express');
+var bodyParser = require('body-parser');
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type' : 'text/plain'});
-  res.end("Hello World!!  I am Shark");
-}).listen(3000);
+var app = express();
+
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(bodyParser.json()); // for parsing application/json
+
+app.get('/', function (req, res){
+  res.send("hello World!! please to meet you");
+});
+
+app.get('/api/greeting', function (req, res) {
+  res.send("Hello. Welcome");
+});
+
+app.post('/api/greeting', function(req, res) {
+  var name = req.body.name;
+  var greeting = req.body.greeting;
+  res.json({name: name, greeting: greeting});
+});
+
+app.get('/api/greeting/:name', function (req, res) {
+  res.send("Hello, "+ req.params.name);
+});
+
+app.listen(3000);
 
 console.log('server starts');
