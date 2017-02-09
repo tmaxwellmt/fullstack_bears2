1d1ff549bbb9033825561fa5e6b5ec5be5c9ba8e
tmaxwellmt <tmaxwellmt@gmail.com>
2017-02-08 21:02:03 -0700

    finished all API endpoints and added and deleted bears

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var Bear = require('./models/bears2');
 var app = express();
 
 var mongoose = require('mongoose');
+
 mongoose.connect("mongodb://localhost/animals");
 
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
@@ -43,6 +44,62 @@ app.post('/api/bears', function(req, res) {
       res.json(bearData);
     }
   })
+});
+
+app.get('/api/bears', function (req, res) {
+  Bear.find(function (err, bearData) {
+    if (err) {
+      console.log("You Can't Code");
+    } else {
+      res.json(bearData);
+    }
+  });
+});
+
+app.get('/api/bears/:bear_id', function (req, res) {
+  var bear_id = req.params.bear_id;
+  Bear.findById(bear_id, function (err, bearData) {
+    if (err) {
+      console.log("The bear with Id:" + bear_id + "was not there");
+    } else {
+      res.json(bearData);
+    }
+  });
+});
+
+app.put('/api/bears/:bear_id', function (req, res) {
+  var bear_id = req.params.bear_id;
+  Bear.findById(bear_id, function (err, bearData) {
+    if (err) {
+      console.log("The bear with Id:" + bear_id + "was not there");
+    } else {
+      var bear = bearData;
+      bear.name = req.body.name ? req.body.name : bear.name;
+      bear.species = req.body.species ? req.body.species : bear.species;
+      bear.weight = req.body.weight ? req.body.weight : bear.weight;
+      bear.age = req.body.age ? req.body.age : bear.age;
+      bear.location = req.body.location ? req.body.location : bear.location;
+      bear.attitude = req.body.attitude ? req.body.attitude : bear.attitude;
+      bear.save(function (err, bearData) {
+        if(err){
+          console.log(err);
+        } else {
+          res.json(bearData);
+        }
+      });
+    }
+  });
+});
+
+app.delete('/api/bears/:bear_id', function(req, res) {
+  var bear_id = req.params.bear_id;
+  Bear.remove({_id: bear_id}, function(err, bear) {
+    if(err){
+      console.log(err, "Could Not Delete Bear");
+    } else {
+      res.json({message: "Bear Deleted", data: bear});
+    }
+  });
 });
 
 app.listen(3000);
